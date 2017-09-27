fdc9d221082a90d23e95765828066e29bb22bfba
bchevali <32142857+bchevali@users.noreply.github.com>
2017-09-27 16:31:32 -0400

    This is Lab 4

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int lab =0;
+
+        while(lab < 4) {
+            System.out.println("This is lab " + lab);
+            lab++;
+
+            if (lab == 4) {
+                System.out.println("Hello this is Lab 4");
+                System.exit(0);
+            }
+
+        }
+
+
+    }
+}
